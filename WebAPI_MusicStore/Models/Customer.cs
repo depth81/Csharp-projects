cdc044cd3c8bd4c3cab059902c43eae99fd3e31e
depth81 <petv1981@gmail.com>
2020-12-13 18:27:31 -0500

    WebApi Music Store
    Momento 3. Lenguaje de Programacion C#. CESDE.

---
+++ WebAPI_MusicStore/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI_MusicStore.Models
+{
+    public class Customer
+    {
+
+        //Introducir la clave primaria manualmente y que no la genere la BD (autoinceremnt)
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+
+        public int ID { get; set; }
+
+        public String FirstName { get; set; }
+
+        public String LastName { get; set; }
+
+        public String Address { get; set; }
+
+        public String City { get; set; }
+
+        public String Country { get; set; }
+
+
+    }
+}
