003aadc0e774414a8027a4081e97e5002a430944
depth81 <petv1981@gmail.com>
2022-05-11 19:40:07 -0500

    update
    ts -> 52:13
    https://www.youtube.com/watch?v=SbVc5YyMRAY&list=PLUp4Ab1lDsFvDrfQyN68SN4VJUwbBu0gu&index=36&t=329s

---
+++ CmsShoppingCart/CmsShoppingCart/Infrastructure/CmsShoppingCartContext.cs
@@ -0,0 +1,19 @@
+﻿using CmsShoppingCart.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Infrastructure
+{
+    public class CmsShoppingCartContext : DbContext
+    {
+        public CmsShoppingCartContext(DbContextOptions<CmsShoppingCartContext> options)
+            :base(options)
+        {
+        }
+
+        public DbSet<Page> Pages { get; set; }
+    }
+}
